c0428b4ab5572e22535e14694b5cd12b331bdc25
Gabriel <gabrielalvesdamas@hotmail.com>
2022-11-11 13:11:23 -0300

    project adjustment

--- src/App.tsx
+++ src/App.tsx
@@ -10,31 +10,14 @@ import { useDemoData } from '@mui/x-data-grid-generator';
 import { DateTimePicker } from '@mui/x-date-pickers-pro';
 import { TextField } from '@mui/material';
 
-LicenseInfo.setLicenseKey('1757b5911b0bfa3a3c1d85b09a103e68Tz01Mjc5NyxFPTE2OTc4MjczOTQ5OTgsUz1wcm8sTE09c3Vic2NyaXB0aW9uLEtWPTI=');
-
-
 function App() {
 
-  const { data } = useDemoData({
-    dataSet: 'Commodity',
-    rowLength: 100000,
-    editable: true,
-  });
 
   return (
     <div>
       <body style={{overflow: 'auto'}}>
       <SideBar/>
       <InovaTrelloQuadro Json={json}/>
-      <DataGridPro
-        {...data}
-        loading={data.rows.length === 0}
-        rowHeight={38}
-        checkboxSelection
-        disableSelectionOnClick
-        experimentalFeatures={{ newEditingApi: true }}
-        sx={{position: 'relative' ,bottom: '0%', marginTop: '9.5%', height: '80%', width: '100%'}}
-      />
       </body>
     </div>
   );
